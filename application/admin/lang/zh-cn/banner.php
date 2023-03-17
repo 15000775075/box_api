837c45b285235e498352eec4637063cdcbbdfb1b
faker <1784913417@qq.com>
2023-03-17 10:59:15 +0800

    init commit

---
+++ application/admin/lang/zh-cn/banner.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Tag'      => '位置',
+    'Tag scsy' => '商城banner',
+    'Tag sczj' => '商城底部',
+    'Name'     => '轮播图名称',
+    'Image'    => '图片',
+    'Weigh'    => '排序',
+    'Bswitch'  => '状态'
+];
